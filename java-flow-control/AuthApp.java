a9d3ebaa49837195060d778923b2c740f1adccec
EunchanJeong <jec65147@gmail.com>
2022-07-06 17:48:51 +0900

    java-flow-control

--- java-flow-control/AuthApp.java
+++ java-flow-control/AuthApp.java
@@ -6,9 +6,15 @@ public class AuthApp {
 		String id = "egoing";
 		String inputId = args[0];
 		
+		String pass = "1111";
+		String pass2 = "2222";
+		String inputPass = args[1];
+		
 		System.out.println("Hi.");
 		
-		if(inputId.equals(id)) {
+		boolean isRightPass = (inputPass.equals(pass) || inputPass.equals(pass2));
+		
+		if(inputId.equals(id) && isRightPass) {
 			System.out.println("Master!");
 		}
 		else {
---
+++ java-flow-control/LogicalOperatorApp.java
@@ -0,0 +1,23 @@
+
+public class LogicalOperatorApp {
+
+	public static void main(String[] args) {
+		
+		// AND
+		System.out.println(true && true);
+		System.out.println(false && true);
+		System.out.println(true && false);
+		System.out.println(false && false);
+
+		// OR
+		System.out.println(true || true);
+		System.out.println(false || true);
+		System.out.println(true || false);
+		System.out.println(false || false);
+		
+		// not
+		System.out.println(!true);
+		System.out.println(!false);
+	}
+
+}
